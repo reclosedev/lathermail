82d5a0e04be38514b3e141e2a76ae127bb369374
Roman Haritonov <rharitonov@asdco.ru>
2015-10-31 16:28:19 +0300

    Bump version 0.1.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 setup(
     name="lathermail",
     url="https://github.com/reclosedev/lathermail/",
-    version="0.1.4",
+    version="0.1.5",
     author="Roman Haritonov",
     description="SMTP Server with API for email testing inspired by mailtrap and maildump",
     author_email="reclosedev@gmail.com",
