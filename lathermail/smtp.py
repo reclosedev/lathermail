204fb389f5872216f838455787426fe8ff14ed4e
Roman Haritonov <rharitonov@asdco.ru>
2015-11-02 20:59:05 +0300

    Properly handle simultaneous connections

--- lathermail/smtp.py
+++ lathermail/smtp.py
@@ -10,13 +10,22 @@ import base64
 log = logging.getLogger(__name__)
 
 
-class SMTPChannelWithAuth(smtpd.SMTPChannel):
+class SMTPChannelWithAuth(smtpd.SMTPChannel, object):
 
-    def __init__(self, server, conn, addr):
+    def __init__(self, server, conn, addr, on_close=lambda s: None):
         smtpd.SMTPChannel.__init__(self, server, conn, addr)
         self.user = None
         self.password = None
         self.fqdn = socket.getfqdn()
+        self._addr = addr
+        self._on_close = on_close
+
+    def close(self):
+        smtpd.SMTPChannel.close(self)
+        try:
+            self._on_close(self._addr)
+        except:
+            log.exception("Exception in on_close for %s:", self._addr)
 
     def smtp_EHLO(self, arg):
         self.push("250-%s\r\n"
@@ -49,21 +58,32 @@ class InboxServer(smtpd.SMTPServer, object):
     def __init__(self, localaddr, handler):
         super(InboxServer, self).__init__(localaddr, None)
         self._handler = handler
-        self._channel = None
+        self._channel_by_peer = {}
 
     def handle_accept(self):
         pair = self.accept()
         if pair is not None:
             conn, addr = pair
             log.info("Incoming connection from %s", repr(addr))
-            self._channel = SMTPChannelWithAuth(self, conn, addr)
+            # Since there is no way to pass channel or additional data (user, password) to process_message()
+            # we have dict of channels indexed by peers. It's cleaned on channel close()
+            self._channel_by_peer[addr] = SMTPChannelWithAuth(self, conn, addr, on_close=self._on_channel_close)
 
     def process_message(self, peer, mailfrom, rcpttos, data):
+        channel = self._channel_by_peer.get(peer)
+        if not channel:
+            log.error("No channel found for peer %s. Channels dump: %s", peer, self._channel_by_peer)
+            return "451 Internal confusion"
+
         log.info("Storing message: inbox: '%s', from: %r, to: %r, peer: %r",
-                 self._channel.user, mailfrom, rcpttos, peer)
+                 channel.user, mailfrom, rcpttos, peer)
         message = email.message_from_string(data)
         return self._handler(to=rcpttos, sender=mailfrom, message=message, body=data,
-                             user=self._channel.user, password=self._channel.password)
+                             user=channel.user, password=channel.password)
+
+    def _on_channel_close(self, peer):
+        self._channel_by_peer.pop(peer, None)
+        log.info("Remove channel for peer: %s. Active channels: %s", peer, len(self._channel_by_peer))
 
 
 def serve_smtp(host="127.0.0.1", port=10252, handler=None):
