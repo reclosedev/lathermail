f49ea8169ecada99ea649cd8fca6bd69279de3f5
Roman Haritonov <rharitonov@asdco.ru>
2017-02-06 22:40:18 +0300

    Bump version 0.4.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,6 +3,12 @@
 Changelog
 ---------
 
+0.4.0 (2017-02-06)
+++++++++++++++++++
+* Add ``_contains`` filters
+* Fix error with simple text format emails
+
+
 0.3.1 (2016-09-02)
 ++++++++++++++++++
 * Proper fix for ``message_from_string`` on Python 2
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 setup(
     name="lathermail",
     url="https://github.com/reclosedev/lathermail/",
-    version="0.3.1",
+    version="0.4.0",
     author="Roman Haritonov",
     description="SMTP Server with API for email testing inspired by mailtrap and maildump",
     author_email="reclosedev@gmail.com",
